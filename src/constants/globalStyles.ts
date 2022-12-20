261f745e9ccc4f5231d309ae21ea7f40ecba33d3
katja.kirchhofer <katja.kirchhofer@birkle-it.com>
2022-12-20 14:11:26 +0100

    add global styles

---
+++ src/constants/globalStyles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+
+const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 10
+  },
+  subTitle: {
+    fontSize: 16,
+    fontWeight: '300',
+    marginVertical: 10
+  },
+  textInput: {
+    width: "100%",
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginTop: 10,
+    marginBottom: 30,
+    borderWidth: 1,
+    borderRadius: 5
+  }
+
+})
+export default globalStyles
